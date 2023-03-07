b3259a8429861c7cc9fe9c770ed89996c58f36db
n509249 <120370027+n509249@users.noreply.github.com>
2023-03-07 08:21:24 +0000

    deleted some resources

--- iac-scripts/s3.tf
+++ iac-scripts/s3.tf
@@ -6,7 +6,5 @@ module "create_s3" {
   server_side_encryption = var.s3[count.index].encryption_enabled
   lifecycle_rules        = var.s3[count.index].lifecycle_rules
   bucket_policy          = var.s3[count.index].bucket_policy
-  sftp_source_bucket     = var.sftp_source_bucket
-  sftp_user_iam_role     = var.sftp_user_iam_role
   tags                   = merge(var.s3[count.index].s3_tags, var.tags)
 }
