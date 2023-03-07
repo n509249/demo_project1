a2e7b79a9268c470e6647ceaf40b28a0f36b9dd9
n509249 <120370027+n509249@users.noreply.github.com>
2023-03-07 08:21:54 +0000

    deleted some resources

--- iac-scripts/sns.tf
+++
@@ -1,7 +0,0 @@
-module "create_sns" {
-  source          = "./batch-modules/modules/sns"
-  sns_name        = var.sns_name
-  display_name    = var.display_name
-  delivery_policy = var.delivery_policy
-  tags            = merge(var.sns_tags, var.tags)
-}
